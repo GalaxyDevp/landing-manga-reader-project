5edfdf07834dabbf57ec7ddf3d2fd17ad58b1e66
lbeltran <leticia.beltran94@gmail.com>
2026-04-02 12:57:33 -0500

    add transition to images in hero

--- src/components/heroImg.jsx
+++ src/components/heroImg.jsx
@@ -5,10 +5,20 @@ import kimetsu from "../assets/images/kimetsu.webp";
 const HeroImg = () => {
   return (
     <div className="flex flex-col gap-y-2">
-      <img src={haikyuu} alt="" className="w-full h-auto rounded-xl" />
+      <img
+        src={haikyuu}
+        alt=""
+        className="w-full h-auto rounded-xl transition duration-150 ease-in-out hover:scale-101"
+      />
       <div className="flex flex-row gap-x-2 md:flex-col md:gap-2 lg:flex-row lg:gap-x-2">
-        <img src={hanako} className="w-full h-auto rounded-xl" />
-        <img src={kimetsu} className="w-full h-auto rounded-xl" />
+        <img
+          src={hanako}
+          className="w-full h-auto rounded-xl transition duration-150 ease-in-out hover:scale-101"
+        />
+        <img
+          src={kimetsu}
+          className="w-full h-auto rounded-xl transition duration-150 ease-in-out hover:scale-101"
+        />
       </div>
     </div>
   );
